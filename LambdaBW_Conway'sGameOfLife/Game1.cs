7ea9e146b6d310387bccaccd3755845029c98089
Eorium <travisgent312@gmail.com>
2020-09-22 15:58:35 -0600

    add grid

--- LambdaBW_Conway'sGameOfLife/Game1.cs
+++ LambdaBW_Conway'sGameOfLife/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Linq;
 
 namespace LambdaBW_Conway_sGameOfLife
 {
@@ -8,6 +10,11 @@ namespace LambdaBW_Conway_sGameOfLife
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D playButton;
+        Texture2D whiteBackground;
+        Texture2D filledGridBox;
+        Texture2D unFilledGridBox;
+        Vector2[,] gridArray = new Vector2[25, 25];
 
         public Game1()
         {
@@ -19,7 +26,17 @@ namespace LambdaBW_Conway_sGameOfLife
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
 
+            for (int x = 0; x < 25; x++)
+            {
+                for (int y = 0; y < 25; y++)
+                {
+                    gridArray[x, y] = new Vector2((x * 25) + 10, (y * 25) + 10);
+                }
+            }
             base.Initialize();
         }
 
@@ -27,14 +44,15 @@ namespace LambdaBW_Conway_sGameOfLife
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            playButton = Content.Load<Texture2D>("playButton");
+            whiteBackground = Content.Load<Texture2D>("whiteBackground");
+            filledGridBox = Content.Load<Texture2D>("filledGridBox");
+            unFilledGridBox = Content.Load<Texture2D>("unfilledGridBox");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -42,7 +60,30 @@ namespace LambdaBW_Conway_sGameOfLife
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(whiteBackground, new Vector2(10, 10), Color.White);
+            _spriteBatch.Draw(
+                playButton, 
+                new Vector2(30, 650), 
+                null, 
+                Color.White, 
+                0, 
+                new Vector2(0, 0), 
+                0.45f, 
+                SpriteEffects.None, 
+                0
+            );
+            for(int x = 0; x < 25; x++)
+            {
+                for(int y = 0; y < 25; y++)
+                {
+                    _spriteBatch.Draw(unFilledGridBox, gridArray[x, y], Color.White);
+                }
+            }
+
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
